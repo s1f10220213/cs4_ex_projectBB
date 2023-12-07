ffa216fc02b1d49f80378e7693fe72f02f592ad7
s1f10220225 <s1F102202257@iniad.org>
2023-12-07 17:13:03 +0900

    daphneの追加(中身はまだ)

---
+++ DevConnect/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.top, name='my_page'),
+    # path('project', views.chat, name='project'),
+    # path('chat', views.chat, name='chat'),
+]
