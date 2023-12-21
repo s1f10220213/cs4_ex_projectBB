3e82275c07fb91aca90d4972353bbce595cd0599
s1f10220213 <107387454+s1f10220213@users.noreply.github.com>
2023-12-21 15:21:08 +0900

    model作った

--- DevConnect/models.py
+++ DevConnect/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 # class User(models.Model):
 #     user_id = models.CharField(max_length=225, unique=True)
 #     user_name = models.TextField()
@@ -9,7 +10,28 @@ from django.db import models
 
 #     def __str__(self):
 #         return self.user_name
+
+# class Project_member(models.Model):
+#     project_id = models.ForeignKey(Project_detail, on_delete=models.CASCADE, related_name="id", null=True) #　プロジェクトのデータベースにアクセス
+#     userList_id = models.ForeignKey(, on_delete=models.CASCADE, related_name="", null=True) #　参加メンバーのデータベースにアクセス
+
+# class Project_detail(models.Model):
+#     id = models.IntegerField()
+#     name = models.CharField(max_length=25)
+#     genre_id = models.ForeignKey(, on_delete=models.CASCADE, related_name="", null=True) #　募集ジャンルのデータベースにアクセス
+#     explain = models.CharField(max_length=255)
+#     leader_id = models.ForeignKey(, on_delete=models.CASCADE, related_name="", null=True) #　リーダーのid
+
+# class Project_Chatroom(models.Model):
+#     id = models.IntegerField()
+#     project_id = models.IntegerField()
+#     name = models.CharField(max_length=15)
+
+# class Project_Genre(models.Model):
+#     project_id = models.ForeignKey(, on_delete=models.CASCADE, related_name="", null=True) #　募集ジャンルのデータベースにアクセス
     
+    
+
 
 # class Listinfo(models.Model):
 #     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name="Genre")
