c5ac66a5a7a516f1db5adafd5a8e395d04154d06
s1f10220225 <s1F102202257@iniad.org>
2024-01-03 23:37:28 +0900

    不足・不要情報の追加・削除

--- DevConnect/models.py
+++ DevConnect/models.py
@@ -39,23 +39,3 @@ from django.db import models
 # class Genres(models.Model):
 #     genre_id = models.IntegerField()
 #     genre_name = models.CharField(max_length=10)
-
-
-# class Listinfo(models.Model):
-#     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, related_name="Genre")
-#     list_num = models.IntegerField()
-#     list_name = models.CharField(max_length=225, null=True)
-
-# class Word(models.Model):
-#     word = models.CharField(max_length=255)
-#     meaning = models.TextField()
-#     belonging_list = models.ForeignKey(Listinfo, on_delete=models.CASCADE, related_name="list_info", null=True)
-
-# class Test(models.Model):
-#     word = models.CharField(max_length=225)
-#     option1 = models.CharField(max_length=225)
-#     option2 = models.CharField(max_length=225)
-#     option3 = models.CharField(max_length=225, null=True)
-#     option4 = models.CharField(max_length=225, null=True)
-#     answer = models.CharField(max_length=225)
-#     correct_answer = models.CharField(max_length=225)
