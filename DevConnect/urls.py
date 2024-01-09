477c5b7cdbc36eda0f6ab1cf8cc60ea760af6bda
s1f10220213 <107387454+s1f10220213@users.noreply.github.com>
2024-01-09 09:23:01 +0900

    detail、searchのurl、views追加

--- DevConnect/urls.py
+++ DevConnect/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('project/cp', views.cp, name='cp'), # CreateProject新規プロジェクト作成
     path('project', views.project, name='project'), # プロジェクトルーム選択
     path('project/<str:project_name>', views.projectin, name='projectin'), # プロジェクトルーム
+    path('projectDetail/<str:project_name>', views.projectDetail, name='projectDetail'), # プロジェクト詳細
+    path('search', views.search, name='search'), # プロジェクト検索
 ]
