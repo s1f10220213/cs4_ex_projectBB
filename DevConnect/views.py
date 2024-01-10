3d934f729f27bc3840bd60bfac439b90221106e3
s1f10220213 <107387454+s1f10220213@users.noreply.github.com>
2024-01-10 20:25:52 +0900

    検索結果ページを作成

--- DevConnect/views.py
+++ DevConnect/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.http import HttpResponse
+from django.http import Http404
+import random
+from DevConnect.models import Project, Project_detail, Project_member_genre, Project_recruit_genre, ProjectMembers, User_Genre, Chat, UserManager, CustomUser
 
 # マイページ
 def mypage(request):
@@ -21,5 +26,12 @@ def projectDetail(request, project_name):
     return render(request, "DevConnect/project_detail.html", {"project_name": project_name})
 
 # プロジェクト検索ページ
-def search(request):
-    return render(request, "DevConnect/search.html")
+def search(request, id):
+    try:
+        detail = Project_detail.objects.get(pk=id)
+    except detail.DoesNotExist:
+        raise Http404("Your page does not exist")
+    context = {
+        "detail": detail
+    }
+    return render(request, "DevConnect/search.html", context)
