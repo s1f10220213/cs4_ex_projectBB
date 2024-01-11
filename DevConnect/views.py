bb87ec38f2cb043352f6d8e29c0faff6305ec3ec
s1f10220213 <107387454+s1f10220213@users.noreply.github.com>
2024-01-11 15:55:50 +0900

    detail修正

--- DevConnect/views.py
+++ DevConnect/views.py
@@ -83,13 +83,10 @@ def chat(request, project_name):
 
 # プロジェクト詳細
 def projectDetail(request, project_name):
-    try:
-        detail = Project.objects.get(name=project_name)
-        members = ProjectMembers.objects.get(project=detail[0].id)
-        genres = Project.objects.get(project=detail[0].id)
+    detail = Project.objects.filter(name=project_name)
+    members = ProjectMembers.objects.filter(project__id=detail[0].id)
+    genres = Genre.objects.filter(project__id=detail[0].id)
 
-    except detail.DoesNotExist:
-        raise Http404("Your page does not exist")
     context = {
         "detail": detail,
         "members": members,
@@ -121,7 +118,7 @@ def search_result(request, genres):
     for i in genre_parts:
         ids = ProjectGenre.objects.filter(genre__genre=i)
         for j in ids:
-            pro = Project.objects.filter(id=j.project)
+            pro = Project.objects.filter(id=j.project.id)
             if pro[0] in projects:
                 continue
             else:
